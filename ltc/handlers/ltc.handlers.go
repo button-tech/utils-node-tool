94c6b08487222c798d0c6bbccc5bbde0bd879ded
prazd <alexokda@list.ru>
2018-12-21 05:33:43 +0300

    add ltc module

---
+++ ltc/handlers/ltc.handlers.go
@@ -0,0 +1,70 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/button-tech/utils-node-tool/ltc/handlers/responseModels"
+	"github.com/gin-gonic/gin"
+	"github.com/imroc/req"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var (
+	ltcURL = os.Getenv("LTC_NODE")
+)
+
+// @Summary LTC balance of account
+// @Description return balance of account in LTC for specific node
+// @Produce  application/json
+// @Param   address        path    string     true        "address"
+// @Success 200 {array} responses.BalanceResponse
+// @Router /ltc/balance/{address} [get]
+// GetBalance return balance of account in LTC for specific node
+func GetBalance(c *gin.Context) {
+
+	address := c.Param("address")
+
+	balance, err := req.Get(ltcURL + "/api/addr/" + address + "/balance")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	balanceFloat, _ := strconv.ParseFloat(balance.String(),64)
+
+	balanceFloat *= 0.00000001
+
+	response := new(responses.BalanceResponse)
+
+	response.Balance = balanceFloat
+
+	c.JSON(http.StatusOK, response)
+
+}
+
+
+// @Summary LTC UTXO of account
+// @Description return UTXO of account
+// @Produce  application/json
+// @Param   address        path    string     true        "address"
+// @Success 200 {array} responses.UTXOResponse
+// @Router /ltc/utxo/{address} [get]
+// GetUTXO return UTXO of account
+func GetUTXO(c *gin.Context){
+
+	address := c.Param("address")
+	utxos, err := req.Get(ltcURL + "/api/addr/" + address + "/utxo")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var respArr []responses.UTXO
+
+	utxos.ToJSON(&respArr)
+
+	response := new(responses.UTXOResponse)
+
+	response.Utxo = respArr
+
+	c.JSON(http.StatusOK, response)
+}
