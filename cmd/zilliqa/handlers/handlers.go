ef2545eaa394cbaca0bcd334b68dffdd82fba889
prazd <alexokda@list.ru>
2019-10-19 23:11:29 +0300

    ...

--- cmd/zilliqa/handlers/handlers.go
+++ cmd/zilliqa/handlers/handlers.go
@@ -1,12 +1,12 @@
 package handlers
 
 import (
+	"errors"
 	"fmt"
 	"github.com/Zilliqa/gozilliqa-sdk/bech32"
 	"github.com/Zilliqa/gozilliqa-sdk/provider"
 	"github.com/button-tech/utils-node-tool/responses"
 	"github.com/qiangxue/fasthttp-routing"
-	"errors"
 )
 
 func GetBalance(c *routing.Context) error {
